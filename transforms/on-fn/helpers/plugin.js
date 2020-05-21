5bd35b5256c8c5fa6f137d466a3aa491fa010e08
Peter Liljegren <pliljegr@pliljegr-mn1.linkedin.biz>
2020-05-21 10:56:11 -0700

    More test cases, better test names

--- transforms/on-fn/helpers/plugin.js
+++ transforms/on-fn/helpers/plugin.js
@@ -1,4 +1,15 @@
+const logger = require('./utils/log');
 const recast = require('ember-template-recast');
+
+const domEvents = [
+  'onclick',
+  'onchange',
+  'onmouseup',
+  'onmouseover',
+  'onmousedown',
+  'onkeyup',
+  'onkeydown',
+];
 
 /**
  * plugin entrypoint
@@ -6,31 +17,53 @@ const recast = require('ember-template-recast');
 function transform(root) {
   let b = recast.builders;
 
+  function convertStringHandler(params) {
+    if (params.length && params[0].type === 'StringLiteral' && params[0].value !== 'click') {
+      logger.info('LOL this is probably fine.');
+      params[0] = b.path(`this.${params[0].original}`);
+    }
+  }
+
+  // NOTE: if the [handler] is a String, it should be converted to this.[string], otherwise left as is
   recast.traverse(root, {
     ElementNode(node) {
-      // modifier (param)
-      node.attributes = node.attributes.map(attr => {
+      /**
+       * action helper:
+       *  - @thing={{action [handler] ...rest}} ==> {{fn [handler] ..rest}}
+       *  - on[event]={{action [handler] ...rest}} ==> {{on "[event]" [handler] ...rest}}
+       *  - on[event]={{action [handler] ...rest value=target.value}} ==> {{on "[event]" [handler] ..rest}}
+       */
+      node.attributes.forEach((attr, index, attributes) => {
         if (attr.value && attr.value.path && attr.value.path.original === 'action') {
           const params = attr.value.params;
-          if (params.length && params[0].type === 'StringLiteral' && params[0].value !== 'click') {
-            params[0] = b.path(`this.${params[0].original}`);
+          convertStringHandler(params);
+          let domEventName = domEvents.filter(e => node.attributes[0].name.includes(e)).pop();
+          if (domEventName) {
+            node.modifiers.push(
+              b.elementModifier(b.path('on'), [b.string(domEventName)].concat(...params))
+            );
+            attributes.slice(index, 1);
+            attributes.length--;
+          } else {
+            attr.value.path = b.path('fn');
           }
-          attr.value.path = b.path('fn');
         }
-        return attr;
       });
-      // action handler
+
+      /**
+       * action modifier
+       *  - default: <div {{action [handler] ...rest }} /> ==> <div {{on "click" [handler]}}
+       *  - defined: <div {{action [handler] ...rest on="[event]"}} /> ==> <div {{on "[event]" [handler] ...rest}}
+       */
       node.modifiers = node.modifiers.map(mod => {
         if (mod.path && mod.path.original === 'action') {
-          const params = mod.params;
           const hash = mod.hash;
+          const params = mod.params;
+          convertStringHandler(params);
           let domEventName = 'click';
           // TODO: this should be look through the list first
           if (hash.pairs.length && hash.pairs[0].key === 'on') {
             domEventName = hash.pairs[0].value.original;
-          }
-          if (params.length && params[0].type === 'StringLiteral' && params[0].value !== 'click') {
-            params[0] = b.path(`this.${params[0].original}`);
           }
           mod = b.elementModifier(b.path('on'), [b.string(domEventName)].concat(...params));
         }
